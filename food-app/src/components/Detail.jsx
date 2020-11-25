9b94bde11e375ba18340c2552f8214085d8eb2b4
mrzacq <hartantoaanan@gmail.com>
2020-11-25 13:02:58 +0700

    done router

--- food-app/src/components/Detail.jsx
+++ food-app/src/components/Detail.jsx
@@ -1,27 +1,40 @@
 import React from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
 function Detail(props) {
   const { collections } = props;
-  const {collection_id} = useParams();
-  const detail = []
-  for(let i = 0; i < collections.length; i++){
-    const obj = collections[i]
-    console.log(obj, 'ini isinya')
-    if(obj.collection.collection_id === collection_id){
-        detail.push(obj.collection)
+  const { collection_id } = useParams();
+  const detail = [];
+  for (let i = 0; i < collections.length; i++) {
+    const obj = collections[i];
+    if (obj.collection.collection_id === +collection_id) {
+      detail.push(obj.collection);
     }
   }
-  console.log(detail, collection_id)
   return (
     <Container className="mt-4">
-          <Card
-            style={{ width: "25rem" }}
-            className="mx-auto my-2"
-          >
-              {detail[0]}
-          </Card>
+      <Card style={{ width: "25rem" }} className="mx-auto my-2">
+        <Row className="mt-3 mb-3">
+          {detail.map((el) => {
+            return (
+              <Card
+                key={el.collection_id}
+                style={{ width: "20rem" }}
+                className="mx-auto my-2"
+              >
+                <Card.Img variant="top" src={el.image_url} />
+                <Card.Body>
+                  <Card.Title>ID: {el.collection_id}</Card.Title>
+                  <Card.Title>{el.title}</Card.Title>
+                  <Card.Text>{el.description}</Card.Text>
+                  <Card.Text><a href={el.description}>Website</a></Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Row>
+      </Card>
     </Container>
   );
 }
