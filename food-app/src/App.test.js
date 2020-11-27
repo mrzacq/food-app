e68929cf6b1c9f157902e99633c0627718156a03
mrzacq <hartantoaanan@gmail.com>
2020-11-27 11:21:23 +0700

    done testing

--- food-app/src/App.test.js
+++ food-app/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('testing app component', () => {
+  test('should render title', () => {
+    const { getByText } = render(<App />);
+    const title = getByText("Food App");
+    expect(title).toBeInTheDocument();
+  });
+})
